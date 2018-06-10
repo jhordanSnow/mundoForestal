9578251317bdc12f5d69065647de6ce147c43047
jhordanSnow <jhordan.gt@gmail.com>
2018-06-10 11:57:48 -0600

    lkenkdas

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 if [ -n "$DYNO" ]  && [ -n "$ENV" ]; then
+    chmod -R 777 frontend/web/
+    chmod -R 777 backend/web/
     php init --env=$ENV --overwrite=All
     php yii migrate/up --interactive=0
     php yii cache/flush-all
